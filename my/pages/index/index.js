36ae7b6f8114946d72679bcdd72878c09b58bf65
huangguangda <2397923107@qq.com>
2018-11-29 07:55:59 +0800

    第一版

--- my/pages/index/index.js
+++ my/pages/index/index.js
@@ -4,22 +4,28 @@ Page({
    * 页面的初始数据
    */
   data: {
-    currentData: 0,
+
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    currentData: 0, 
+    selectPerson: true,
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  },
+  onLoad: function(options) {},
   //获取当前滑块的index
-  bindchange: function (e) {
+  bindchange: function(e) {
     const that = this;
     that.setData({
       currentData: e.detail.current
     })
   },
   //点击切换，滑块index赋值
-  checkCurrent: function (e) {
+  checkCurrent: function(e) {
     const that = this;
 
     if (that.data.currentData === e.target.dataset.current) {
