f025a1a21f3a877f54b4fd216a1f861fcac9aa19
huangguangda <2397923107@qq.com>
2018-12-14 03:25:38 +0800

    第一版

--- my/pages/jl/jl.js
+++ my/pages/jl/jl.js
@@ -1,138 +1,271 @@
-// pages/jl/jl.js
+var time = 0;
+var touchDot = 0; //触摸时的原点
+var interval = "";
+var flag_hd = true;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    title: '',
-    cate: '',
-    account: '',
-    modalHidden: true
+    font: "",
+    arr: [],
+    sysW: null,
+    lastDay: null,
+    firstDay: null,
+    weekArr: ['日', '一', '二', '三', '四', '五', '六'],
+    year: null,
+    day: null,
+    ballTop: 0,
+    ballLeft: 0,
+    screenHeight: 0,
+    screenWidth: 0,
+    text: "没有滑动",
   },
-  //   标题文本框
-  bindTitleInput: function(e) {
+  onShow: function() {
+    flag_hd = true; //重新进入页面之后，可以再次执行滑动切换页面代码
+    clearInterval(interval); // 清除setInterval
+    time = 0;
+  },
+  test: function(event) {
+
+    let pageX = event.touches[0].pageX;
+    let pageY = event.touches[0].pageY;
+    if (pageX < 30 || pageY < 30)
+      return;
+    if (pageX > this.data.screenWidth - 30)
+      return;
+    if (pageY > this.data.screenHeight - 30)
+      return;
     this.setData({
-      title: e.detail.value
-    })
-    console.log(e.detail.value)
+      ballTop: event.touches[0].pageY - 30,
+      ballLeft: event.touches[0].pageX - 30,
+    });
+
   },
-  //   金额radio
-  radioChange: function(e) {
-    this.setData({
-      cate: e.detail.value
-    })
-    console.log(e.detail.value)
-  },
-  //   金额文本框  
-  bindAccountInput: function(e) {
-    this.setData({
-      account: e.detail.value
-    })
-    console.log(e.detail.value)
-  },
-  save: function() {
-    var that = this
-    // 本条数据打包成json
-    var record = {
-      title: this.data.title,
-      cate: this.data.cate,
-      account: this.data.account,
+  next: function(res) {
+
+    if (this.data.endDay < this.data.getDate) {
+
+      this.onLoad(res, this.data.year, this.data.month, this.data.endDay, 2)
+    } else if (this.data.lastDay == this.data.getDate && this.data.endDay != null) {
+      this.onLoad(res, this.data.year, Number(this.data.month), 1, 2)
+    } else if (this.data.lastDay != this.data.endDay) {
+
+      this.onLoad(res, this.data.year, Number(this.data.month - 1), Number(this.data.endDay + 1), 2)
+    } else {
+
+      this.data.endDay = 0
+      this.onLoad(res, this.data.year, Number(this.data.month), 1, 2)
     }
-    var data = []
+  },
+  last: function(res) {
+    if (this.data.startDay < 7) {
+      this.onLoad(res, this.data.year, this.data.month - 1, this.data.startDay - 1)
+    } else if (this.data.startDay > this.data.endDay && this.data.font == 1) {
+      this.onLoad(res, this.data.year, this.data.month - 2, this.data.startDay)
+    } else {
+      this.onLoad(res, this.data.year, this.data.month - 1, this.data.startDay - 7)
+    }
+  },
+  //获取日历相关参数
+  dataTime: function(year, month, day, state) {
 
-    wx.getStorage({
-      key: 'db',
+    var last = this.data.lastDay
+    var date = new Date(year, month, day);
+    if (year == null) {
+      date = new Date(2018, 3, 28)
+    }
+    var year = date.getFullYear();
+    var month = date.getMonth();
+    var day = date.getDate();
+    var months = date.getMonth() + 1;
+
+    //获取现今年份
+    this.data.year = year;
+
+    //获取现今月份
+    this.data.month = months;
+
+    //获取今日日期
+    this.data.getDate = date.getDate();
+
+    //最后一天是几号
+    var d = new Date(year, months, 0);
+
+    this.data.lastDay = d.getDate();
+    //第一天星期几
+
+    let firstDay
+    if (state == 1) {
+
+      if (d.getDate() == date.getDate()) {
+        if (this.data.startDay == 1) {
+          firstDay = new Date(year, Number(month), Number(new Date(year, month - 1, 0).getDate() - 6));
+        } else if (this.data.startDay <= 7) {
+
+          firstDay = new Date(year, Number(month), Number(new Date(year, month - 1, 0).getDate() - 8));
+        } else {
+          firstDay = new Date(year, Number(month), Number(new Date(year, month - 1, 0).getDate() - 7));
+        }
+
+      } else if (this.data.startDay >= 6) {
+
+        if (month == 11 && this.data.endDay > this.data.startDay && this.data.startDay != 16 && this.data.startDay != 9) {
+          if (this.data.endDay <= 23) {
+
+            firstDay = new Date(year, Number(month + 1), day);
+          } else {
+
+            firstDay = new Date(year, Number(month), day);
+          }
+
+        } else if (this.data.startDay == 8) {
+          firstDay = new Date(year, Number(month), day - 4);
+        } else {
+          if (this.data.startDay == 6 && this.data.year != 2018 && this.data.month != 4) {
+            this.data.month = Number(this.data.month + 1)
+            firstDay = new Date(year, Number(month + 1), day);
+          } else if (this.data.year == 2018 && this.data.month == 4) {
+            firstDay = new Date(year, Number(month), day);
+          } else {
+            firstDay = new Date(year, Number(month - 1), day);
+          }
+
+        }
+
+      } else {
+        if (this.data.startDay == 4 && month == 2) {
+
+          firstDay = new Date(year, Number(month - 1), Number(new Date(year, month - 1, 0).getDate() - day + 1));
+
+        } else if (month == 1) {
+
+          firstDay = new Date(year, Number(month), Number(new Date(year, month - 1, 0).getDate() - day + 2));
+        }
+
+      }
+    } else if (state == 2) {
+
+      if (this.data.endDay == last && last != null) {
+
+        firstDay = new Date(year, month, 1);
+
+      } else {
+        firstDay = new Date(year, Number(month), Number(1 + this.data.endDay));
+      }
+    } else {
+      firstDay = new Date(year, month, day);
+    }
+    this.data.firstDay = firstDay.getDay();
+    this.setData({
+      getWeek: date.getDay()
+    })
+  },
+  touchStart: function(e) {
+    touchDot = e.touches[0].pageX; // 获取触摸时的原点
+    // 使用js计时器记录时间    
+    interval = setInterval(function() {
+      time++;
+    }, 100);
+  },
+  // 触摸结束事件
+  touchEnd: function(e) {
+    var touchMove = e.changedTouches[0].pageX;
+    // 向左滑动   
+    if (touchMove - touchDot <= -20 && time < 10) {
+
+      //执行切换页面的方法
+      this.next()
+    }
+    // 向右滑动   
+    if (touchMove - touchDot >= 20 && time < 10) {
+
+      //执行切换页面的方法
+      this.last()
+    }
+    clearInterval(interval); // 清除setInterval
+    time = 0;
+
+  },
+  onLoad: function(options, year, month, day, state) {
+    var _this = this;
+    wx.getSystemInfo({
       success: function(res) {
-        console.log('db:' + res.data)
-        data = res.data
-        // 取出本地数据
-        data.push(record)
-        // 存回本地
-        wx.setStorage({
-          key: 'db',
-          data: data
-        })
-
-        // 提示框
-        that.setData({
-          modalHidden: false
+        _this.setData({
+          screenHeight: res.windowHeight,
+          screenWidth: res.windowWidth,
+          font: ""
         });
       }
-    })
-  },
+    });
+    this.dataTime(year, month, day, state);
+    var two;
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {
+    //根据今天是星期几，几号获得周的日期
+    var res = wx.getSystemInfoSync();
+    var date = ""
+    if (this.data.getWeek == 0) {
+      date = this.data.getDate
+    } else if (this.data.getDate <= this.data.getWeek) {
+      two = 1
+      date = 1
+    } else {
+      date = this.data.getDate - Number(this.data.getWeek)
+    }
+    var num = Number(this.data.getDate + (6 - this.data.getWeek));
 
-  },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
+    if (num > this.data.lastDay) {
+      num = this.data.lastDay
+    }
 
-  },
+    var cha = Number(this.data.lastDay - date)
+    var endDay;
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-    wx.getStorage({
-      key: 'db',
-      // 初始加载无数据，故插入一条空数组的新key
-      fail: function() {
-        // 存回本地
-        wx.setStorage({
-          key: 'db',
-          data: []
-        })
+
+    this.data.arr = []
+    var startDay = date;
+    if (two == 1) {
+      this.setData({
+        font: 1
+      })
+      var last = new Date(this.data.year, this.data.month - 1, 1).getDay()
+      var start = new Date(this.data.year, this.data.month - 1, 0).getDate()
+      var now = start - last + 1
+      var newdate = new Date(this.data.year, this.data.month - 1, now)
+      startDay = newdate.getDate()
+      for (var i = startDay; i <= start; i++) {
+        this.data.arr.push(i);
       }
-    })
-  },
-  // 关闭对话框
-  hideAlertView: function() {
+    }
+
+    for (var i = date; i <= num; i++) {
+
+      this.data.arr.push(i);
+      endDay = i;
+    }
+
+    if (cha < 6) {
+      this.setData({
+        font: 2
+      })
+      for (var i = 1; i <= (6 - cha); i++) {
+        this.data.arr.push(i);
+        endDay = i;
+      }
+    }
+
+
     this.setData({
-      'modalHidden': true
-    })
-    // 返回上一页
-    wx.navigateBack()
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
+      sysW: res.windowHeight / 12, //更具屏幕宽度变化自动设置宽度
+      marLet: this.data.firstDay,
+      arr: this.data.arr,
+      year: this.data.year,
+      getDate: this.data.getDate,
+      month: this.data.month,
+      endDay: endDay,
+      startDay: startDay
+    });
   }
 })
